3d80998a7362a8c1a0e7785d8b4594898d395762
onionknight621 <onionknight621@gmail.com>
2022-08-13 13:17:34 +0300

    chore: prettify

--- constants.js
+++ constants.js
@@ -4,16 +4,16 @@ export const MODES = {
 };
 
 export const CONSTRAINTS = {
-    video: {
-      width: {
-        min: 640,
-        ideal: 1920,
-        max: 2560,
-      },
-      height: {
-        min: 640,
-        ideal: 1080,
-        max: 1440
-      },
-    }
-  };
+  video: {
+    width: {
+      min: 640,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 640,
+      ideal: 1080,
+      max: 1440,
+    },
+  },
+};
